a84d3ee3c76b1c83ab2aeaad05449589a643c7bb
ArtJDev <artemkurilenko1983@yandex.ru>
2022-09-06 09:19:10 +0300

    Initial commit

---
+++ src/main/java/ru/netology/config/JavaConfig.java
@@ -0,0 +1,23 @@
+package ru.netology.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.profile.DevProfile;
+import ru.netology.profile.ProductionProfile;
+import ru.netology.profile.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+    @Bean
+    @ConditionalOnProperty(prefix = "netology", name = "profile.dev")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "netology", name = "profile.prod")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
